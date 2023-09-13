81ea7e9aee9aa6565240495999cd282495a5fcd0
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-09-13 12:43:21 +0500

    добавлена конфигурация

---
+++ src/components/Counter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "CounTer",
+};
+</script>
+
+<template>
+  <div>
+    <h1>Counterr</h1>
+  </div>
+</template>
+
+<style scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 const fs = require("fs");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: '0.0.0.0',
+    hot: true,
+    host: "0.0.0.0",
     port: 8080,
-    server : {
-
-      type: 'https' ,
-      options: process.env.HTTPS_MODE? {
-        key: fs.readFileSync('.certs/key.pem'),
-        cert: fs.readFileSync('.certs/cert.pem'),
-      }: {},
+    server: {
+      type: "https",
+      options: {
+        key: fs.readFileSync(".certs/key.pem"),
+        cert: fs.readFileSync(".certs/cert.pem"),
+      },
     },
-  }
-})
+  },
+});
